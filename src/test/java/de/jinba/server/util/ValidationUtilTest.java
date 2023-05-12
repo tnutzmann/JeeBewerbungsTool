b75138540a625ae0feaca91ac293b814da0f1ab0
maxriedel03 <max@m-riedel.de>
2023-05-12 17:02:56 +0200

    Reformat

--- src/test/java/de/jinba/server/util/ValidationUtilTest.java
+++ src/test/java/de/jinba/server/util/ValidationUtilTest.java
@@ -16,76 +16,91 @@ class ValidationUtilTest {
         assertTrue(ValidationUtil
                 .isValidPassword("1aB!56789"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithoutNumber() {
         assertFalse(ValidationUtil
                 .isValidPassword("aB!abcdefgh"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithoutUppercase() {
         assertFalse(ValidationUtil
                 .isValidPassword("1a!abcdefgh"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithoutLowercase() {
         assertFalse(ValidationUtil
                 .isValidPassword("1A!ABCDEFGH"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithoutSpecialCharacter() {
         assertFalse(ValidationUtil
                 .isValidPassword("1aBcdefghij"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithLessThan8Characters() {
         assertFalse(ValidationUtil
                 .isValidPassword("1aB!abc"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithMoreThan20Characters() {
         assertFalse(ValidationUtil
                 .isValidPassword("1aB!abcdefghijabcdefghij"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithWhitespace() {
         assertFalse(ValidationUtil
                 .isValidPassword("1aB!abc def"));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithNull() {
         assertFalse(ValidationUtil
                 .isValidPassword(null));
     }
+
     @Test
     void shouldReturnFalseForPasswordWithEmptyString() {
         assertFalse(ValidationUtil
                 .isValidPassword(""));
     }
+
     @Test
     void shouldReturnTrueForValidEmail() {
         assertTrue(ValidationUtil
                 .isValidEmail("abc@abc.de"));
     }
+
     @Test
     void shouldReturnFalseForEmailWithoutAt() {
         assertFalse(ValidationUtil
                 .isValidEmail("abcabc.de"));
     }
+
     @Test
     void shouldReturnFalseForEmailWithoutDot() {
         assertFalse(ValidationUtil
                 .isValidEmail("abc@abcde"));
     }
+
     @Test
     void shouldReturnFalseForEmailWithWhitespace() {
         assertFalse(ValidationUtil
                 .isValidEmail("abc@abc de"));
     }
+
     @Test
     void shouldReturnFalseForEmailWithNull() {
         assertFalse(ValidationUtil
                 .isValidEmail(null));
     }
+
     @Test
     void shouldReturnFalseForEmailWithEmptyString() {
         assertFalse(ValidationUtil
