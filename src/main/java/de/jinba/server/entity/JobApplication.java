55d4d2bc7de3f2da916230e50acc10ce864d9b26
maxriedel03 <max@m-riedel.de>
2023-06-14 21:45:15 +0200

    Added new Entity for Applications and some Fields

---
+++ src/main/java/de/jinba/server/entity/JobApplication.java
@@ -0,0 +1,31 @@
+package de.jinba.server.entity;
+
+import de.jinba.server.entity.enumuration.ApplicationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @ManyToOne
+    private JobOffer jobOffer;
+    @ManyToOne
+    private AppUser applicant;
+    @Enumerated(EnumType.ORDINAL)
+    private ApplicationStatus status;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
