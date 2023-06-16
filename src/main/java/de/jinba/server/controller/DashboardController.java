9c36b6ef5d34f4d49d780de8f5157aba51115d2f
tnutzmann <tonynutzmann@icloud.com>
2023-06-16 10:20:00 +0200

    profile displayed on dashboard

--- src/main/java/de/jinba/server/controller/DashboardController.java
+++ src/main/java/de/jinba/server/controller/DashboardController.java
@@ -4,6 +4,7 @@ import de.jinba.server.entity.AppUser;
 import de.jinba.server.service.AppUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,15 @@ public class DashboardController {
     private final AppUserDetailsService appUserDetailsService;
     @GetMapping("/dashboard")
     public String viewDashboard(Model model) {
-        AppUser currentUser = appUserDetailsService.getCurrentAuthenticatedUser()
+        AppUser profile = appUserDetailsService.getCurrentAuthenticatedUser()
                 .orElseThrow(() -> new IllegalStateException("Unauthenticated user attempted to access profile page"));
+        boolean isOwnProfile = profile.getUsername().equals(SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName());
+        profile.getSkills().sort((o1, o2) -> o2.getLevel().ordinal() - o1.getLevel().ordinal());
+        model.addAttribute("profile", profile);
+        model.addAttribute("isOwnProfile", isOwnProfile);
         return "dashboard";
     }
 
