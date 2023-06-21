8fe0b00e3afa43935b5e58a0dcfe10d67f4941be
tnutzmann <tonynutzmann@icloud.com>
2023-06-21 14:28:52 +0200

    merge Conversations resolved

--- src/main/java/de/jinba/server/controller/DashboardController.java
+++ src/main/java/de/jinba/server/controller/DashboardController.java
@@ -4,7 +4,6 @@ import de.jinba.server.entity.AppUser;
 import de.jinba.server.service.AppUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +17,7 @@ public class DashboardController {
     public String viewDashboard(Model model) {
         AppUser profile = appUserDetailsService.getCurrentAuthenticatedUser()
                 .orElseThrow(() -> new IllegalStateException("Unauthenticated user attempted to access profile page"));
-        boolean isOwnProfile = profile.getUsername().equals(SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName());
-        profile.getSkills().sort((o1, o2) -> o2.getLevel().ordinal() - o1.getLevel().ordinal());
         model.addAttribute("profile", profile);
-        model.addAttribute("isOwnProfile", isOwnProfile);
         return "dashboard";
     }
 
