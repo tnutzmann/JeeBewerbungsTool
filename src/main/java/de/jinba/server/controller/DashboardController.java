17c2f9e19cb0fe8b07e88fd9875eaf79ea3da987
tnutzmann <tonynutzmann@icloud.com>
2023-06-24 18:22:07 +0200

    500 error

--- src/main/java/de/jinba/server/controller/DashboardController.java
+++ src/main/java/de/jinba/server/controller/DashboardController.java
@@ -1,23 +1,38 @@
 package de.jinba.server.controller;
 
 import de.jinba.server.entity.AppUser;
+import de.jinba.server.entity.JobOffer;
+import de.jinba.server.entity.enumuration.Role;
 import de.jinba.server.service.AppUserDetailsService;
+import de.jinba.server.service.CompanyDetailsService;
+import de.jinba.server.util.ModelConfigurer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.stream.Collectors;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class DashboardController {
     private final AppUserDetailsService appUserDetailsService;
+    private final CompanyDetailsService companyDetailsService;
     @GetMapping("/dashboard")
     public String viewDashboard(Model model) {
         AppUser profile = appUserDetailsService.getCurrentAuthenticatedUser()
                 .orElseThrow(() -> new IllegalStateException("Unauthenticated user attempted to access profile page"));
         model.addAttribute("profile", profile);
+
+        if(profile.getRole().equals(Role.COMPANY_USER)) {
+            ModelConfigurer.of(model)
+                    .with("applications", companyDetailsService.findCompanyOfCurrentUser().getJobOffers().stream()
+                            .map(JobOffer::getApplications).collect(Collectors.toList()));
+            return "dashboard";
+        }
+
         return "dashboard";
     }
 
