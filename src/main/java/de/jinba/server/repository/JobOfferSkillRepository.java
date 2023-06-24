f791103165371369db668115bcf9a24636dadaaf
maxriedel03 <max@m-riedel.de>
2023-06-24 17:11:09 +0200

    Document Repositories

--- src/main/java/de/jinba/server/repository/JobOfferSkillRepository.java
+++ src/main/java/de/jinba/server/repository/JobOfferSkillRepository.java
@@ -4,6 +4,9 @@ import de.jinba.server.entity.JobOfferSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * This repository provides DB operations for {@link JobOfferSkill}.
+ */
 @Repository
 public interface JobOfferSkillRepository extends JpaRepository<JobOfferSkill, Long> {
 }
