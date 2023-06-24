c42d57833a6aa4aa152c2368edecf84a76dc37ff
maxriedel03 <max@m-riedel.de>
2023-06-24 17:11:09 +0200

    Document Repositories

--- src/main/java/de/jinba/server/repository/AppUserRepository.java
+++ src/main/java/de/jinba/server/repository/AppUserRepository.java
@@ -6,9 +6,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * This repository provides DB operations for {@link AppUser}.
+ */
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
+    /**
+     * This method finds an {@link AppUser} by its email.
+     * @param email The email of the {@link AppUser}.
+     * @return The {@link AppUser} with the given email.
+     */
     Optional<AppUser> findByEmail(String email);
 
+    /**
+     * This method checks if an {@link AppUser} with the given email exists.
+     * @param email The email of the {@link AppUser}.
+     * @return True if an {@link AppUser} with the given email exists, false otherwise.
+     */
     boolean existsByEmail(String email);
 }
--- src/main/java/de/jinba/server/repository/CompanyRepository.java
+++ src/main/java/de/jinba/server/repository/CompanyRepository.java
@@ -7,11 +7,30 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * This repository provides DB operations for {@link Company}.
+ */
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, String> {
+    /**
+     * This method finds a {@link Company} by its name.
+     * @param name The name of the {@link Company}.
+     * @return The {@link Company} with the given name.
+     */
     Optional<Company> findByName(String name);
 
+    /**
+     * This method checks if a {@link Company} with the given name exists.
+     * @param name The name of the {@link Company}.
+     * @return True if a {@link Company} with the given name exists, false otherwise.
+     */
     boolean existsByName(String name);
+
+    /**
+     * This method finds a {@link Company} by its admin.
+     * @param id The id of the admin.
+     * @return The {@link Company} with the given admin.
+     */
     Optional<Company> findByAdmin_Id(String id);
 
 }
