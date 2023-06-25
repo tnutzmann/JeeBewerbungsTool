dfaf96e0cb3cb8482a602711f93d7e4375fac75f
maxriedel03 <max@m-riedel.de>
2023-06-25 13:12:36 +0200

    When user authenticated: redirect to dashboard automatically

--- src/main/java/de/jinba/server/controller/AuthenticationController.java
+++ src/main/java/de/jinba/server/controller/AuthenticationController.java
@@ -1,5 +1,8 @@
 package de.jinba.server.controller;
 
+import de.jinba.server.service.AppUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  * This controller handles the custom login page. It does not handle the login itself.
  */
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController {
+    private final AppUserDetailsService appUserDetailsService;
     /**
      * Shows the login page.
      *
@@ -15,6 +21,9 @@ public class AuthenticationController {
      */
     @GetMapping("/login")
     public String viewLoginPage() {
+        if(appUserDetailsService.isAuthenticated()){
+            return "redirect:/dashboard";
+        }
         return "login";
     }
 }
